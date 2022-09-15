e69347aff209ca8dd67d0934ea58a8e25255a8f7
NaveenChandu29 <82800083+NaveenChandu29@users.noreply.github.com>
2022-09-15 11:43:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import AxiosCall from "./Components/AxiosCall";
+import "./styles.css";
+
+function App() {
+  return <AxiosCall />;
+}
+
+export default App;
---
+++ src/Components/AxiosCall/index.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+import Axios from "axios";
+
+class AxiosCall extends Component {
+  state = {
+    axiosData: []
+  };
+
+  componentDidMount() {
+    this.dataCall();
+  }
+
+  dataCall = () => {
+    Axios.get("https://jsonplaceholder.typicode.com/posts").then((res) =>
+      this.setState({ axiosData: res.data })
+    );
+  };
+
+  render() {
+    const { axiosData } = this.state;
+    axiosData.map((eachItem) => console.log(eachItem));
+    return (
+      <div>
+        <h1>Axios Call</h1>
+      </div>
+    );
+  }
+}
+
+export default AxiosCall;
